96915d7339973244b10ac622f2a800384581b168
Steeven1 <alfredsteveen.456@gmail.com>
2026-04-23 10:56:50 -0500

    comment a

--- calculadora-sanciones-spdp/js/app.js
+++ calculadora-sanciones-spdp/js/app.js
@@ -391,3 +391,4 @@ function buildRangeInfoText(institutionType, infractionType, rdmRange) {
   const maxStr = Number(rdmRange.max).toFixed(2);
   return `Rango aplicado (${model} - ${infractionLabel}): ${minStr} a ${maxStr} ${unit}.`;
 }
+//a
